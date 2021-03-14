00ebb1fa528e090889ca25620b48c586aaa9ebf1
Umer Farooq <itsmeumer96@gmail.com>
2021-03-14 14:16:39 +0500

    date and time

--- src/components/Date.js
+++ src/components/Date.js
@@ -1,32 +1,30 @@
-import React from 'react';
-import '@mobiscroll/react/dist/css/mobiscroll.min.css';
-import { Datepicker, Page, setOptions } from '@mobiscroll/react';
+import React from "react";
+import "@mobiscroll/react/dist/css/mobiscroll.min.css";
+import { Datepicker, Page, setOptions } from "@mobiscroll/react";
 
 setOptions({
-    theme: 'ios',
-    themeVariant: 'light'
+  theme: "ios",
+  themeVariant: "light",
 });
 
 function Date() {
-    const inputProps = {
-        placeholder: 'Please Select...'
-    };
-    
-    const boxInputProps = {
-        label: 'Range',
-        labelStyle: 'stacked',
-        inputStyle: 'outline',
-        placeholder: 'Please Select...'
-    };
-    
-    return (
-        <Page>
-            <Datepicker controls={['date']} inputComponent="input" inputProps={inputProps} />
-            <Datepicker controls={['date']} inputProps={boxInputProps} />
-            <Datepicker controls={['date']}  display="inline"/>
-        </Page>
-    );
-}
+  const inputProps = {
+    placeholder: "Please Select...",
+  };
 
+  const boxInputProps = {
+    label: "Range",
+    labelStyle: "stacked",
+    inputStyle: "outline",
+    placeholder: "Please Select...",
+  };
+
+  return (
+    <Page>
+      <Datepicker display="anchored" touchUi={true} />
+      <Datepicker display="anchored"  controls={["time"]} timeFormat="h:mm A" touchUi={true} />
+    </Page>
+  );
+}
 
 export default Date;
